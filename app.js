264c6d29228eba0b5c68e17d9ecec89fd4776f6f
Saumyadip-Kar <saumyadipkar@gmail.com>
2024-07-02 19:46:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -724,7 +724,7 @@ function getRandomColor() {
     for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
-    color += "CC"; //opacity - 80%
+    color += "80"; //opacity - 80%
     return color;
 }
 
